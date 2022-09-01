060770413634898d9b3712f2a314361f556efa97
mkingg_mm <mkingg@email.com>
2022-09-01 23:11:35 +0800

    add actuator add discovery add enable self

--- cloud-provider-payment8002/src/main/java/com/mkingg/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/mkingg/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.mkingg.springcloud.entities.Payment;
 import com.mkingg.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author mkingg
@@ -23,6 +26,9 @@ public class PaymentController {
 
     @Resource
     private PaymentService paymentService;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult create(@RequestBody Payment payment) {
@@ -44,6 +50,21 @@ public class PaymentController {
         } else {
             return new CommonResult<>(444, "查询失败，查询ID：" + id, null);
         }
+    }
+
+    @GetMapping("/payment/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("service: " + service);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId() + "\t" + instance.getInstanceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+
+        }
+        return this.discoveryClient;
     }
 
 }
