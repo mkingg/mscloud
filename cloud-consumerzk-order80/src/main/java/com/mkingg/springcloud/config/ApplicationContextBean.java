da78ae75b90bb8b0e55439d3174c696591c658ae
mkingg_lv <mkingg@email.com>
2022-09-05 15:21:36 +0800

    add cloud-consumerzk-order80

---
+++ cloud-consumerzk-order80/src/main/java/com/mkingg/springcloud/config/ApplicationContextBean.java
@@ -0,0 +1,20 @@
+package com.mkingg.springcloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author mkingg
+ * @Description:
+ */
+@Configuration
+public class ApplicationContextBean {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
---
+++ cloud-consumerzk-order80/src/main/java/com/mkingg/springcloud/controller/OrderZKController.java
@@ -0,0 +1,29 @@
+package com.mkingg.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @author mkingg
+ * @Description:
+ */
+@RestController
+@Slf4j
+public class OrderZKController {
+
+    public static final String INVOKE_URL = "http://cloud-provider-payment";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/zk")
+    public String PaymentInfo(){
+        String result = restTemplate.getForObject(INVOKE_URL + "/payment/zk", String.class);
+        log.info("消费者调用支付服务（zookeeper）--->\t" + result);
+        return result;
+    }
+}
