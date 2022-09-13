a353592001be394f778fd932d408419d07a5728f
mkingg_mm <mkingg@email.com>
2022-09-13 22:26:40 +0800

    add orderGlobalFallbackMethod to OrderHystrixController.java

--- cloud-consumer-feign-hystrix-order80/src/main/java/com/mkingg/springcloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/mkingg/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,7 @@
 package com.mkingg.springcloud.controller;
 
 import com.mkingg.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
@@ -16,26 +17,32 @@ import javax.annotation.Resource;
  */
 @RestController
 @Slf4j
+@DefaultProperties(defaultFallback = "orderGlobalFallbackMethod")
 public class OrderHystrixController {
 
     @Resource
     private PaymentHystrixService paymentHystrixService;
 
     @GetMapping("/consumer/payment/hystrix/ok/{id}")
-    public String paymentInfoOK(@PathVariable("id") Integer id){
+    public String paymentInfoOK(@PathVariable("id") Integer id) {
         return paymentHystrixService.paymentInfoOK(id);
 
     }
 
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
-    @HystrixCommand(fallbackMethod = "paymentInfoTimeOutHandler", commandProperties = {
-            @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="1500")
-    })
-    public String paymentInfoTimeOut(@PathVariable("id")Integer id){
+//    @HystrixCommand(fallbackMethod = "paymentInfoTimeOutHandler", commandProperties = {
+//        @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+//    })
+    @HystrixCommand //加了@DefaultProperties属性注解，并且没有写具体方法名字，就用统一全局的
+    public String paymentInfoTimeOut(@PathVariable("id") Integer id) {
         return paymentHystrixService.paymentInfoTimeOut(id);
     }
 
     public String paymentInfoTimeOutHandler(Integer id) {
         return "线程池:" + Thread.currentThread().getName() + "o(╥﹏╥)o\t我是消费者80,对方支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己";
+    }
+
+    public String orderGlobalFallbackMethod() {
+        return "Global 异常处理信息，请稍后再试， /( ㄒ o ㄒ )/~~";
     }
 }
